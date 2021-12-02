404ffa6ae0048332f7f38ff3f9b4262edd9b48a6
Allenzzp <131452abc@gmail.com>
2021-12-02 03:16:55 +0000

    my implementation

--- wordsearch.js
+++ wordsearch.js
@@ -3,6 +3,20 @@ const wordSearch = (letters, word) => {
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    const row = letters.length;
+    const col = letters[0].length;
+    const verticalJoin = [];
+    for (let j = 0; j < col; j++) {
+        let wd = "";
+        for (let i = 0; i < row; i++) {
+            wd += letters[i][j];
+        }
+        verticalJoin.push(wd);
+    }
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true
+    }
+    return false;
 }
 
 module.exports = wordSearch
